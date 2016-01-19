f9b7923ffca6bf79360663126146d0f37e888a4e
armandogb <armandogbulnesjr@gmail.com>
2016-01-18 22:38:50 -0600

    did some about page styling

--- compliance.php
+++ compliance.php
@@ -4,7 +4,7 @@
 
 			<section class="spread-banner main-pad">
 				<div class="banner blurb">
-					<h2>Don't just compete. Prosper.</h2>
+					<h1>Don't just compete. Prosper.</h1>
 					<p>These days, regulations change fast. We don't just help you 
 keep up—we help you concentrate on the growth of your CU.</p>
 				</div>
@@ -20,19 +20,29 @@ keep up—we help you concentrate on the growth of your CU.</p>
 			</section>
 			<section class="box-section">
 				<div class="link-box">
-					
+					<h3>MEMBER ACCOUNT</h3>
+					<p>Deposit, Fiduciary and Electronic Payments</p>
+					<a href="/">VIEW ALL</a>
 				</div>
 				<div class="link-box">
-					
+					<h3>LENDING</h3>
+					<p>Loans, Leasing, Credit Cards and Real Estate</p>
+					<a href="/">VIEW ALL</a>
 				</div>
 				<div class="link-box">
-					
+					<h3>OPERATIONS</h3>
+					<p>Advertising, BSA, Employment, and Legal</p>
+					<a href="/">VIEW ALL</a>
 				</div>
 				<div class="link-box">
-					
+					<h3>GOVERNANCE</h3>
+					<p>Board, Continuity Planning, Field of Membership and more</p>
+					<a href="/">VIEW ALL</a>
 				</div>
 				<div class="link-box">
-					
+					<h3>IT</h3>
+					<p>Social media security and seven branches of cybersecurity</p>
+					<a href="/">VIEW ALL</a>
 				</div>
 			</section>
 			
