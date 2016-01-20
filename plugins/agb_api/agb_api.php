033b3927b18f85aeff75e3caa49bf464d604e3c0
Armando Bulnes <armandogbulnesjr@gmail.com>
2016-01-20 11:23:46 -0600

    mvp of api plugin

--- plugins/agb_api/agb_api.php
+++ plugins/agb_api/agb_api.php
@@ -9,16 +9,115 @@
 		Author Email: armandogb@cliquestudios.com
 		License:n/a
 		*/
+?>
+
+<?php
+
+	function user_check(){
+
+    	if ( is_user_logged_in() == true ) {
+
+    		if(isset($_POST['xxx'])) { 
+
+				class PopPosts{
+
+
+					private $parsed_data =[];
+					private $headers=[];
+					private $id_holder =[];
+					private $final=[];
+
+
+					function __construct($url){
+
+						add_action('init', array( &$this, self::run_it(str_replace('https://', 'http://', $url))));
+					}
+
+
+					function get_data($url){
+
+						$data=[];
+						$csv = file_get_contents($url);
+						$lines = explode(PHP_EOL, $csv);
+
+						 foreach ($lines as $line) {
+			   				 $data[] = str_getcsv($line);
+						}
+
+						$this->headers = array_slice($data, 0,1);
+						unset($data[0]);
+						$this->parsed_data = array_values($data);	
+
+
+					}
+
+					function key_data($array){
+
+						foreach($array as $arr){
+
+							$copy = $this->headers[0];
+							$result = array_combine($copy, $arr);
+							array_push($this->final, $result);
+
+						}
+
+					}
+
+					function fill_pages($array){
+
+						$origin_id = 'field_569fb579aaf7c';
+
+				/*		$args = array(
+							'post_type'        => 'api_page',
+							'post_status'      => 'publish'						
+							);
+
+						$query = get_posts($args);
+
+						foreach($query as $entry){
+							array_push($this->id_holder, the_field( $origin_id,$entry['ID'] ));
+						}*/
+
+						foreach($array as $arr){
+							$post = array(
+							  'post_content'   => file_get_contents(str_replace('https://', 'http://', $arr['APILink'])),
+							  'post_title'     => $arr['PageName'],
+							  'post_type'        => 'api_page',
+							  'post_status'    => 'publish' 
+							);
+
+							$mode = /*in_array($arr['ID'], $this->id_holder) ?*/ wp_insert_post($post)/* : wp_update_post($post)*/;
+
+							update_field($origin_id, $arr['ID'], $mode); 
+						}
+
+
+					}
+
+					function run_it($e){
+						self::get_data($e);
+						self::key_data($this->parsed_data);
+						self::fill_pages($this->final);
+					}
+
+
+				}
+
+				new PopPosts($_POST["xxx"]);
+			} 
+
+    	} 
+	}
+
+	add_action('init','user_check');
+	
+		
 
 	class AgbApi{
 
 		const name = 'Agb Api';
 		const slug = 'agb_api';
 
-		private $parsed_data =[];
-		private $headers=[];
-		private $id_holder =[];
-		private $final=[];
 
 		function __construct(){
 
@@ -26,11 +125,6 @@
 
 			add_action( 'init', array( &$this, 'init_agb_api' ));
 			
-
-			$end_point = esc_attr(get_option(AgbApi::slug.'_endpoint'));
-
-	/*		self::run_it($end_point);*/
-
 		}
 
 		function init_agb_api(){
@@ -62,74 +156,25 @@
 					<table class="form-table">
 						<tr>
 							<th>EndPoint</th>
-							<td><input style="width:350px;" type="text" name="<?php echo self::slug.'_endpoint' ?>" placeholder="URL" value="<?php echo esc_attr(get_option(self::slug.'_endpoint')) ?>" /></td>
+							<td><input style="width:450px;" type="text" name="<?php echo self::slug.'_endpoint' ?>" placeholder="URL" value="<?php echo esc_attr(get_option(self::slug.'_endpoint')) ?>" /></td>
 						</tr>
 					</table>
 					<?php submit_button(); ?>
+					</form>
+					<form method="post" action="<?=$_SERVER['PHP_SELF'];?>">
+						<input type="hidden" name="xxx" value="<?php echo esc_attr(get_option(self::slug.'_endpoint')) ?>">
+						<input style="width:110px; height:50px; color:white; background:red; border-radius:5px;"type="submit" name="go" value="Populate" />
+						<h3>(This may take a few minutes)</h3>
 					</form>
 				</div>
 			<?php
 		}
 
-		function get_data($url){
-
-			$data=[];
-			$csv = file_get_contents($url);
-			$lines = explode(PHP_EOL, $csv);
-
-			 foreach ($lines as $line) {
-   				 $data[] = str_getcsv($line);
-			}
-
-			$this->headers = array_slice($data, 0,1);
-			unset($data[0]);
-			$this->parsed_data = array_values($data);	
-
-
-		}
-
-		function key_data($array){
-
-			foreach($array as $arr){
-
-				$copy = $this->headers[0];
-				$result = array_combine($copy, $arr);
-				array_push($this->final, $result);
-
-			}
-
-		}
-
-		function fill_pages($array){
-
-			foreach($array as $arr){
-
-				$post = array(
-				  'post_content'   => file_get_contents(str_replace('https://', 'http://', $arr['APILink'])),
-				  'post_title'     => $arr['PageName'],
-				  'post_status'    => 'publish' ,
-				  'origin_id'      =>  $arr['ID']
- 
-				);
-
-				wp_insert_post($post); 
-			}
-
-
-		}
-
-		function run_it($e){
-			self::get_data($e);
-			self::key_data($this->parsed_data);
-			self::fill_pages($this->final);
-		}
-
-
 
 	}
 
-	
 
-new AgbApi();
+	new AgbApi();
+
 
 ?>
