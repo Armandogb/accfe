f344ffac752d74204445eaf9f816ec8060e7c25a
armandogb <armandogbulnesjr@gmail.com>
2016-01-19 21:14:13 -0600

    added some plugin logic

--- plugins/agb_api/agb_api.php
+++ plugins/agb_api/agb_api.php
@@ -15,11 +15,21 @@
 		const name = 'Agb Api';
 		const slug = 'agb_api';
 
+		private $parsed_data =[];
+		private $headers=[];
+		private $id_holder =[];
+		private $final=[];
+
 		function __construct(){
 
 			register_activation_hook( __FILE__, array( &$this, 'install_agb_api' ));
 
 			add_action( 'init', array( &$this, 'init_agb_api' ));
+			
+
+			$end_point = esc_attr(get_option(AgbApi::slug.'_endpoint'));
+
+	/*		self::run_it($end_point);*/
 
 		}
 
@@ -34,54 +44,92 @@
 
 
 		function menu_boot() {
-			add_menu_page( 'Agb Api', 'Agb Api', 'manage_options', 'agb-api', array(&$this, 'agb_api_user_page'),'<div id="icon-edit" class="icon32"></div>');
+			add_menu_page( 'Agb Api', 'Agb Api', 'manage_options', 'agb_api',array(&$this, 'agb_api_user_page'),'dashicons-thumbs-down');
 		}
 
 		function register_options() {
 			register_setting( self::slug."_settings", self::slug."_endpoint");
 		}
 
+		function agb_api_user_page(){
+			?>
+
+				<div class="wrap">
+					<h1><?php echo self::name ?> Settings</h1>
+					<form method="post" action="options.php">
+					<?php settings_fields( self::slug."_settings" ); ?>
+					<?php do_settings_sections(  self::slug."_settings" ); ?>
+					<table class="form-table">
+						<tr>
+							<th>EndPoint</th>
+							<td><input style="width:350px;" type="text" name="<?php echo self::slug.'_endpoint' ?>" placeholder="URL" value="<?php echo esc_attr(get_option(self::slug.'_endpoint')) ?>" /></td>
+						</tr>
+					</table>
+					<?php submit_button(); ?>
+					</form>
+				</div>
+			<?php
+		}
+
+		function get_data($url){
+
+			$data=[];
+			$csv = file_get_contents($url);
+			$lines = explode(PHP_EOL, $csv);
+
+			 foreach ($lines as $line) {
+   				 $data[] = str_getcsv($line);
+			}
+
+			$this->headers = array_slice($data, 0,1);
+			unset($data[0]);
+			$this->parsed_data = array_values($data);	
+
+
+		}
+
+		function key_data($array){
+
+			foreach($array as $arr){
+
+				$copy = $this->headers[0];
+				$result = array_combine($copy, $arr);
+				array_push($this->final, $result);
+
+			}
+
+		}
+
+		function fill_pages($array){
+
+			foreach($array as $arr){
+
+				$post = array(
+				  'post_content'   => file_get_contents(str_replace('https://', 'http://', $arr['APILink'])),
+				  'post_title'     => $arr['PageName'],
+				  'post_status'    => 'publish' ,
+				  'origin_id'      =>  $arr['ID']
+ 
+				);
+
+				wp_insert_post($post); 
+			}
+
+
+		}
+
+		function run_it($e){
+			self::get_data($e);
+			self::key_data($this->parsed_data);
+			self::fill_pages($this->final);
+		}
+
+
+
 	}
 
-	$url = "http://il.leagueinfosight.com/client/infosight/il/list.php";
-
-	$csv = file_get_contents($url);
-	$lines = explode(PHP_EOL, $csv);
- 	$data=[];
- 	$final_pages = [];
-	$id_holder =[1];
-	$final=[];
-
- 	foreach ($lines as $line) {
-   	 $data[] = str_getcsv($line);
-	}
 	
-	$headers = array_slice($data, 0,1);
-	unset($data[0]);
-	$parsed_data = array_values($data);
 
-
-	foreach($parsed_data as $array){
-
-		$copy = $headers[0];
-		$result = array_combine($copy, $array);
-
-		array_push($final, $result);
-	}
-
-
-	foreach($final as $entry){
-
-		if(!in_array($entry['ID'],$id_holder)){
-			
-			array_push($id_holder, $entry['ID']);
-			array_push($final_pages, $entry);
-
-		};
-	}
-
-/*	str_replace('https://', 'http://', $final[0]['APILink'] );*/
-
-
+new AgbApi();
 
 ?>
